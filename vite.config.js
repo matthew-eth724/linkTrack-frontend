2c392ef9b3e06605239288db6dae2c48ee7e9791
irene ayakazi <fuosirene2014@gmail.com>
2026-02-08 01:47:55 +0100

    sorted shi

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDotenv } from 'dotenv'
+
+configDotenv()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +10,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: 'process.env.backend_url',
                 changeOrigin: true,
             }
         }
