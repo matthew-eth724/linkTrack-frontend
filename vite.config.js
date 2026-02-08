adbd610210b781c705c1fe462f37fd3568063c0d
irene ayakazi <fuosirene2014@gmail.com>
2026-02-08 02:36:34 +0100

    sorted more shit wo

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { configDotenv } from 'dotenv'
-configDotenv()
-import { defineConfig } from 'vite'
-
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,12 +7,9 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'process.env.backend_url',
+                target: 'http://localhost:3000',
                 changeOrigin: true,
             }
-        }, watch: {
-            usePolling: true,
-            interval: 300
         }
     }
 })
